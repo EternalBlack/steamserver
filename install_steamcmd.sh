e41dd32b7e4c95104a5f991e58f7b77894eb3d6d
Markus Black <Bittner@EternalBlack.com>
2017-08-15 21:03:21 +0200

    different order

--- install_steamcmd.sh
+++ install_steamcmd.sh
@@ -14,12 +14,12 @@ rm $STEAMCMD_FILE
 echo "Executing server."
 cd ~/server
 args=()
-args+=( '-autoupdate' )
-args+=( '-console' )
 (( ${#GAME_TYPE} >= 1 )) && args+=( '-game $GAME_TYPE' )
 (( ${#WORKSHOP_KEY} >= 1 )) && args+=( '-authkey $WORKSHOP_KEY' )
 (( ${#WORKSHOP_ID} >= 1 )) && args+=( '+host_workshop_collection $WORKSHOP_ID' )
 (( ${#MAX_PLAYERS} >= 1 )) && args+=( '+maxplayers $MAX_PLAYERS' )
 (( ${#GAME_MODE} >= 1 )) && args+=( '+gamemode $GAME_MODE' )
 (( ${#MAP} >= 1 )) && args+=( '+map $MAP' )
+args+=( '-autoupdate' )
+args+=( '-console' )
 ./srcds_run "${args[@]}"
